8ad21224d2ecd2a967d44fa0b7f2367d090f18ad
Jason Piao <jason.piao@mail.utoronto.ca>
2016-10-22 17:36:47 -0400

    pod file and UI

--- McKinseyHACKS/ViewController.swift
+++ McKinseyHACKS/ViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate { // inherit for hitting return
+    
+    @IBOutlet weak var textfield1: UITextField!
+    @IBOutlet weak var textfield2: UITextField!
+    @IBOutlet weak var textfield3: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+         // for hitting return
+        textfield1.delegate = self
+        textfield2.delegate = self
+        textfield3.delegate = self
+        
+         // for tapping outside keyboard
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard)))
+        
+    }
+    
+    // for tapping outside keyboard
+    func dismissKeyboard() {
+        textfield1.resignFirstResponder()
+        textfield2.resignFirstResponder()
+        textfield3.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    // for hitting return
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
