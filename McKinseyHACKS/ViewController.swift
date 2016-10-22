cbc9f09db50e52a19bf9185baf6fe1b769ab0fe7
Jason Piao <jason.piao@mail.utoronto.ca>
2016-10-22 15:17:48 -0400

    Initial Commit

---
+++ McKinseyHACKS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  McKinseyHACKS
+//
+//  Created by Jason Piao on 2016-10-22.
+//  Copyright © 2016 Jason Piao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
